f112991b1385e74ad10bb03081d7d06665a5642f
Adorabus <adorabusfox@gmail.com>
2022-09-14 19:33:03 +1000

    trp framework support

---
+++ client/framework/trp.lua
@@ -0,0 +1,63 @@
+if GetResourceState('thrillerp') == 'missing' then return end
+
+local inv = exports['trp-inventory']
+
+local Job = 0
+local JobGrade = 0
+local jobSecond = {}
+
+RegisterNetEvent("Thrille:JobUpdated")
+AddEventHandler("Thrille:JobUpdated", function(job, grade)
+    Job = job
+    JobGrade = grade
+end)
+
+RegisterNetEvent("Thrille:JobUpdatedSecond")
+AddEventHandler("Thrille:JobUpdatedSecond", function(data)
+  jobSecond = data
+end)
+
+function GroupName(groupid)
+    local name = "Error Retrieving Name"
+    local mypasses = jobSecond
+    for i=1, #mypasses do
+        if mypasses[i]["jobid"] == groupid then
+            name = mypasses[i]["business_name"]
+        end
+    end
+    return name
+end
+
+function GroupRank(groupid)
+    local rank = 0
+    local mypasses = jobSecond
+    for i=1, #mypasses do
+        if mypasses[i]["jobid"] == groupid then
+            rank = mypasses[i]["grade"]
+        end
+    end
+    return rank
+end
+
+function PlayerHasGroups(filter)
+    local _type = type(filter)
+    local grade = filter.grade or 1
+
+    if filter.job then
+        return Job == filter.job and JobGrade >= grade
+    elseif filter.company then
+        return GroupRank(filter.company) >= grade
+    end
+end
+
+function PlayerHasItems(filter)
+    local _type = type(filter)
+
+    if _type == 'string' then
+        return inv:HasItem(filter)
+    elseif _type == 'table' then
+        return inv:HasItems(filter)
+    end
+
+    return true
+end
